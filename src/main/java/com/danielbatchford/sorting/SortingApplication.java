bb392775abd70070f3e5be71fe5618ae5e28ed7a
Daniel Batchford (Elly) <danielbatchford@gmail.com>
2020-06-07 21:12:58 +0100

    Reorganised Code Base

--- src/main/java/com/danielbatchford/sorting/SortingApplication.java
+++ src/main/java/com/danielbatchford/sorting/SortingApplication.java
@@ -3,9 +3,20 @@ package com.danielbatchford.sorting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 @SpringBootApplication
 @RestController
@@ -16,7 +27,4 @@ public class SortingApplication {
 	}
 
 
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
-	}
 }
