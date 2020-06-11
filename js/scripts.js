41de4a1d0814654a266163fdb6047777c73fea26
Daniel Batchford (Elly) <danielbatchford@gmail.com>
2020-06-11 02:32:47 +0100

    Added in file handling, reading and data cleaning

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,33 @@
+//Called when clicking visual button. Adds FileReader event listener and clicks hidden html file button
 function openAttachment() {
+  document.getElementById('attachment').addEventListener('change', readFile, false);
   document.getElementById('attachment').click();
 }
 
-function fileSelected(input){
+//Displays the file input and test button.
+function fileSelected(input) {
   document.getElementById('btnAttachment').innerHTML = input.files[0].name;
-  document.getElementById('testBtn').style.display = 'inline-block';
 }
 
-function computeData(){
-console.log("Hello World")
+//Read From a file and display the interpreted result
+function readFile(evt) {
+
+  var f = evt.target.files[0];
+  if (f) {
+    var r = new FileReader();
+
+    r.onload = function(e) {
+      var stringData = e.target.result;
+
+      document.getElementById('readInfo').style.display = "block";
+      document.getElementById('tableContainer').style.display = "block";
+
+        stringData = stringData.replace(/[^\d.-]/g, ' ');
+        document.getElementById("readResult").innerHTML = stringData;
+    }
+    r.readAsText(f);
+  }
+  else {
+    document.getElementById("btnAttachment").innerHTML = "Cannot Read From This File";
+  }
 }
