222fbd453a65015688bd7390e06fd2fd934d4619
Daniel Batchford (Elly) <danielbatchford@gmail.com>
2020-06-11 13:05:11 +0100

    Visual overhaul, some algorithms implemented with display on the table now working. Modal also added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+
+window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+    
+function showModal(){
+  document.getElementById('modal').style.display = "block";
+}
 //Called when clicking visual button. Adds FileReader event listener and clicks hidden html file button
 function openAttachment() {
   document.getElementById('attachment').addEventListener('change', readFile, false);
@@ -19,12 +29,25 @@ function readFile(evt) {
     r.onload = function(e) {
       var stringData = e.target.result;
 
-      document.getElementById('readInfo').style.display = "block";
-      document.getElementById('tableContainer').style.display = "block";
+      document.getElementById('readResultContainer').style.display = "block";
 
-      stringData = stringData.replace(/\D/g, ' ').replace(/\s\s+/g, ' '); //Clean up this regex
-      document.getElementById("readResult").innerHTML = stringData;
-      tableBuilder(stringData.split(' ').map(Number));
+
+      data = stringData.replace(/\D/g, ' ').replace(/\s\s+/g, ' ').split(' ').map(Number); //Clean up this regex, work with neg numbers
+
+
+      var stringToDisplay = "";
+      var maxToDisplay = 15;
+
+      for (var i = 0, cap = Math.min(data.length, maxToDisplay); i < cap; i++) {
+        stringToDisplay += data[i] + ", ";
+      }
+      if (data.length > maxToDisplay) {
+        stringToDisplay += " ...";
+      }
+
+      document.getElementById("readResult").innerHTML = stringToDisplay;
+      document.getElementById("fileInfo").innerHTML = "Found " + data.length +" Elements. Your input was interpreted as:";
+      tableBuilder(data);
     }
     r.readAsText(f);
 
@@ -35,5 +58,67 @@ function readFile(evt) {
 }
 
 function tableBuilder(data) {
-  console.log(data);
+    document.getElementById('tableContainer').style.display = "block";
+    document.getElementById("table").rows[1].cells[3].innerHTML = quickSort(data,performance.now());
+    document.getElementById("table").rows[2].cells[3].innerHTML = mergeSort(data,performance.now());
+    document.getElementById("table").rows[3].cells[3].innerHTML = bubbleSort(data,performance.now());
+    document.getElementById("table").rows[4].cells[3].innerHTML = selectionSort(data,performance.now());
+  document.getElementById("table").rows[5].cells[3].innerHTML = insertionSort(data,performance.now());
+
+  console.log(insertionSort(data, performance.now()));
+}
+
+function timeFormat(input){
+  if(input > 1){
+    return input.toFixed(1)+" ms";
+  }
+  return (input*1000).toFixed(1)+" ns";
+}
+
+function bubbleSort(data, timeStarted) {
+
+  var length = data.length;
+  var swapped = true;
+  while (swapped) {
+    swapped = false;
+    for (var i = 0; i < length - 1; i++) {
+      if (data[i] > data[i + 1]) {
+        var tmp = data[i];
+        data[i] = data[i + 1];
+        data[i + 1] = tmp;
+        swapped = true;
+      }
+    }
+  }
+
+  return timeFormat(performance.now() - timeStarted);
+}
+
+function quickSort(data, timeStarted) {
+    return timeFormat(performance.now() - timeStarted);
+}
+
+function insertionSort(data, timeStarted) {
+
+  var length = data.length;
+
+  for(var i = 1; i < length; i++){
+    var key = data[i];
+    var j = i-1;
+    while(j >= 0 && data[j] > key){
+      data[j+1] = data[j];
+      j--;
+    }
+      data[j+1] = key;
+  }
+
+    return timeFormat(performance.now() - timeStarted);
+}
+
+function mergeSort(data, timeStarted) {
+    return timeFormat(performance.now() - timeStarted);
+}
+
+function selectionSort(data, timeStarted) {
+    return timeFormat(performance.now() - timeStarted);
 }
